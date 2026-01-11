2e402be20e937667de4acb70fbbfc78dc47262b6
Jenny-high <okihomotmik@gmail.com>
2026-01-11 17:55:34 +0900

    JavaScriptが対応するようにした

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import { createTypeReferenceDirectiveResolutionCache } from "typescript";
-import "./styles.css";
-
 const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
